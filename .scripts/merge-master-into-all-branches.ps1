64c548b4587bc1933c4846ab60ad292b11bdb847
calin-darie <calin_darie@yahoo.com>
2016-07-30 20:29:58 +0300

    adding scripts to manage branches. , useful to teachers only. -create-local-branches.ps1  for each remote branch, sets up a local branch to track it. useful when first setting up a local repository. - merge-master-into-all-branches: after an exercise is added, merge the change to all student branches.

---
+++ .scripts/merge-master-into-all-branches.ps1
@@ -0,0 +1,17 @@
+cd "$PSScriptRoot\.."
+
+$branches = git for-each-ref refs/remotes/origin --format='%(refname)' | `
+		foreach {	$_ -replace "refs/remotes/origin/", ""}
+$branches | `
+		foreach { 
+		  $branch = $_
+			Write-Host "merging master into $branch..."
+			git checkout $branch
+			git merge master
+			if (-not $?) { 
+				Write-Host "An error occured. Solve it, then rerun this script." 
+				break		
+			}
+		}
+
+git checkout master
