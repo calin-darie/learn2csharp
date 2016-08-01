31379dab3900128568bd73df92075aa78752e788
AndreeaMazere <andreea.mazere@cegeka.com>
2016-08-01 14:14:08 +0300

    welcome1

--- 01.HelloWorld/HelloWorld/Program.cs
+++ 01.HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Hello, Andreea!");
             Console.ReadKey();
         }
     }
