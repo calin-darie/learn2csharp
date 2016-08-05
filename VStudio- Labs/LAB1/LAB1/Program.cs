4091bfe1956841a92884f0b92d69fe7350424cf5
Roxana Neagu <Roxana.Neagu@cegeka.com>
2016-08-05 14:19:39 +0300

    First Exercise

---
+++ VStudio- Labs/LAB1/LAB1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float A = 26;
+            float B = 7;
+            var myResult = A / B;
+
+
+            Console.WriteLine("The Result is" +" " + myResult);
+            Console.ReadKey();
+
+
+        }
+    }
+}
