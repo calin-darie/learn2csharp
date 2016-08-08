234a0c40f867c605ef60167e1348d2ab34adb31c
AlexVladCGK <vlad.dana.alexandra@gmail.com>
2016-08-08 11:28:49 +0300

    new Alex :)

--- 01.HelloWorld/HelloWorld/Program.cs
+++ 01.HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Alex!");
+            Console.WriteLine("Hello, happy_Alex!");
             Console.ReadKey();
         }
     }
