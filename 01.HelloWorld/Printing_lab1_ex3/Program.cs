5fd14ee3fdc1d26a76d5da40df19c8a6a0433323
AlexVladCGK <vlad.dana.alexandra@gmail.com>
2016-08-08 12:40:15 +0300

    ex3from lab 1

---
+++ 01.HelloWorld/Printing_lab1_ex3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printing_lab1_ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kilometers = 10; 
+            double time = 1.5; 
+            bool truth = true; 
+            Console.WriteLine(@"{0} has ran {1} kilometers in {2} hours. It is {3} that he's a family man.He takes his brother out for a drink at a bar called Gigis Han. He and his wife are going to have a son. His name will be {0}, just like his father's. {0} dreams to get a diploma from his son saying '{0}, father of the year'", "Gigi", kilometers, time, truth);
+            Console.ReadKey();
+        }
+    }
+}
