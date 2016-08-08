f0adbf3fa9d787fa75902d836911cacd11881432
AndreeaMazere <andreea.mazere@cegeka.com>
2016-08-08 14:25:59 +0300

    Divisio exercise 1-2

---
+++ 01.HelloWorld/division/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int divident = 26;
+            int divisor = 7;
+             Console.WriteLine (  divident/ divisor);
+
+
+             double divident2 = 26;
+             double divisor2 = 7;
+             Console.WriteLine(divident2 / divisor2);
+
+             Console.WriteLine(divident2 % divisor2);
+        }
+        
+    }
+}
