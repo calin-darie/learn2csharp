77ad6e2476bda46ed9cbae3d7b1252bb87599bdc
AndreeaMazere <andreea.mazere@cegeka.com>
2016-08-18 09:43:06 +0300

    Andreea - if01

--- 02.ProgramFlow/If01/Program.cs
+++ 02.ProgramFlow/If01/Program.cs
@@ -4,23 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace If01
+namespace if01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Your age:");
-            string ageString = Console.ReadLine();
-            int age = int.Parse(ageString);
+            Console.Write ("What is your age?");
+            string ageAsString = (Console.ReadLine());
+            int age = int.Parse(ageAsString);
+            Console.WriteLine("What is your gender?");
+            var gender = (Console.ReadLine());
+        
 
-            Console.WriteLine("Your sex:"); //male or female
-            string sex = Console.ReadLine();
+            if (( age > 24) && (gender == "male"))
+            {
+                Console.WriteLine("You are a valid person.");
+            }
 
-            //write here your solution
+            else
+            {
+                Console.WriteLine("You are an invalid person.");
+            }
 
+            if ((age > 20) && ( age < 32) && (gender == "female"))
+            {
+                Console.WriteLine("You are a valid person.");
+            }
+            }
 
-            Console.ReadKey();
         }
     }
-}
+
