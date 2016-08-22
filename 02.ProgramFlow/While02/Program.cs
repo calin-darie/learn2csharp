38b0e62a9271d8c5bfe152e55bbb26603fe6969f
AlexVladCGK <vlad.dana.alexandra@gmail.com>
2016-08-22 13:18:00 +0300

    ex 4 lab2

---
+++ 02.ProgramFlow/While02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int sum = 0;
+            int nr;
+            Console.WriteLine("enter natural numbers:");
+            do
+            {                
+                string nrString = Console.ReadLine();
+                nr= int.Parse(nrString);
+                sum = sum + nr;
+            }
+            while (nr != 0);
+            Console.WriteLine("this is the final sum:");
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
