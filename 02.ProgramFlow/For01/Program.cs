0033abc56805dda33de2669730f3f4d48ca46aef
AlexVladCGK <vlad.dana.alexandra@gmail.com>
2016-08-22 14:32:29 +0300

    ex 6 lab 2

---
+++ 02.ProgramFlow/For01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a nr bigger than 0:");
+            string user_nr_String = Console.ReadLine();
+            int user_nr = int.Parse(user_nr_String);
+            Console.WriteLine("the following nr are even in the given interval:");
+
+            for (int i=0; i<user_nr; i=i+2)
+            { 
+                if (i%2==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
