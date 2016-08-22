5d6f4ba50e4811b1594455477e1ece88ae1b2acd
AlexVladCGK <vlad.dana.alexandra@gmail.com>
2016-08-22 11:49:42 +0300

    rezolvare cu intrebari exercitiu 1 lab2

--- 02.ProgramFlow/If01/Program.cs
+++ 02.ProgramFlow/If01/Program.cs
@@ -13,14 +13,25 @@ namespace If01
             Console.WriteLine("Your age:");
             string ageString = Console.ReadLine();
             int age = int.Parse(ageString);
-
             Console.WriteLine("Your sex:"); //male or female
             string sex = Console.ReadLine();
 
             //write here your solution
+            if (age >= 20 && age <= 32)
+            {
+                if (sex == "male")
+                { Console.WriteLine("Valid person! this is a 24 years male"); }
+                else
+                { Console.WriteLine("Valid person!this is a young female :P"); }
+            }
 
+            else
+            { Console.WriteLine("Invalid person!"); }
 
             Console.ReadKey();
+            // ar mai trebui tratate si exceptiile: cand se introduce formatul gresit de la tastatura
+            // intrebari: se poate si fara {}?
+
         }
     }
 }
