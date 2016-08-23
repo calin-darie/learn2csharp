5f5eeb8559cf8d115b6c27fbd438e4d7404f656e
AndreeaMazere <andreea.mazere@cegeka.com>
2016-08-23 10:37:57 +0300

    While02 ready with help from Roxy

---
+++ 02.ProgramFlow/while02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type numbers from 0,10");
+            int number;
+            int sum;
+            sum = 0;
+
+            do
+            {
+                string readValue = Console.ReadLine();
+                number = int.Parse(readValue);
+                sum = sum + number;
+
+                //Console.WriteLine("new sum is" + sum);
+            }
+            while (number != 0);
+
+            Console.WriteLine("Sum is" + sum);
+            
+            Console.ReadKey();
+
+          /*  
+            {
+                   number = (number + number + number + number + number + number);
+                    }
+            while (number <= 10 && number >= 0);
+           */
+        }
+    }
+}
