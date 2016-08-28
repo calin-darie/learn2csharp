2168afa759f8d50aa56e40add17009c263b29a06
mihaeladima <mihaela.dima@cegeka.com>
2016-08-28 17:30:49 +0300

    Commit exercise 3 Lab 1 (Gigi)

---
+++ 01.HelloWorld/FormattingTemplate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormattingTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kilometers = 10; 
+            double time = 1.5;
+            bool truth = true;
+            string name = "Gigi";
+
+            Console.WriteLine("{0} has ran {1} kilometers in {2} hours. It is {3} that he's a family man." +
+            "He takes his brother out for a drink at a bar called Gigis Han. He and his wife are going to have a son." +
+            "His name will be {0}, just like his father's. {0} dreams to get a diploma from his son saying '{0}, father of the year'.", name, kilometers, time, truth);
+        }
+    }
+}
