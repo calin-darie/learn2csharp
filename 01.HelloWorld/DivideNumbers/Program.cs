4825fa251b2d9c941309ef4f6cce6a8dacbf54ac
mihaeladima <mihaela.dima@cegeka.com>
2016-08-28 17:02:16 +0300

    commit DivideNumbers

---
+++ 01.HelloWorld/DivideNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quotient = 26 / 7;
+            int remainder = 26 - quotient * 7;
+            double quotientdec = 26.0 / 7;
+            Console.WriteLine("Quotient: " + quotient);
+            Console.WriteLine("Remainder: " + remainder);
+            Console.WriteLine("Quotient with decimals: " + quotientdec);
+        }
+    }
+}
