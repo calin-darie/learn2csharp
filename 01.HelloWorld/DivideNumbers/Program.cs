897efcb9bcd050cf387cad6e99d4eee57481a168
andreioprea <andrei.oprea@cegeka.com>
2016-08-29 15:43:15 +0300

    Finished course 1

---
+++ 01.HelloWorld/DivideNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quotient = 26 / 7;
+            int remainder = 26 % 7;
+            double decimals = 26.0 / 7;
+
+            Console.WriteLine(quotient);
+            Console.WriteLine(remainder);
+            Console.WriteLine(decimals.ToString());
+        }
+    }
+}   
