a11557c9870c1ba42f9962b0ae234c594b3b8aac
AlexVladCGK <vlad.dana.alexandra@gmail.com>
2016-08-29 14:45:11 +0300

    ex 10 lab 2

--- 02.ProgramFlow/Switch01/Program.cs
+++ 02.ProgramFlow/Switch01/Program.cs
@@ -10,7 +10,28 @@ namespace Switch01
     {
         static void Main(string[] args)
         {
+            Console.Write("please enter a person name:");
+            string user_name_String = Console.ReadLine();
 
+            switch (user_name_String)
+            {
+                case "calin":
+                    Console.WriteLine("Calin e smecher");
+                    break;
+                case "paul":
+                    Console.WriteLine("Paul e de treaba");
+                    break;
+                case "alex":
+                    Console.WriteLine("Alex e fain");
+                    break;
+                case "teo":
+                    Console.WriteLine("Teo e si mai fain :P");
+                    break;
+                default:
+                    Console.WriteLine("nu-l cunosc pe " + user_name_String);
+                    break;
+            }
+            Console.ReadKey();
         }
     }
 }
