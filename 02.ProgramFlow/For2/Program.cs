ec0341c1f2259cae7ed42519d2c80326a1958655
AlexVladCGK <vlad.dana.alexandra@gmail.com>
2016-08-29 14:20:06 +0300

    Update Ex For2 lab

--- 02.ProgramFlow/For2/Program.cs
+++ 02.ProgramFlow/For2/Program.cs
@@ -11,28 +11,16 @@ namespace For2
         static void Main(string[] args)
         {     
           
-            for (int i=0; i<=40; i=i+1)
+            for (int i=0; i<=20; i=i+1)
             {
-                for (int j = 0; j <= 40; j = j + 1)
-                {   
-                    if (i <20 && j<20)
-                    {
-                        Console.WriteLine("these are the pairs from zero to 20:");
-                        Console.WriteLine("({0}, {1})", i, j);
-                    }
-                    else if(i==20 && j==20)
-                    {
-                        Console.WriteLine("({0}, {1})", i, j);
-                        Console.WriteLine("these are the pairs from 20 to 40:");
-                        Console.WriteLine("({0}, {1})", i, j);
-                    }
-                    else
-                    { Console.WriteLine("({0}, {1})", i, j); }
+                for (int j = 20; j <= 40; j = j + 1)
+                {
+                    Console.WriteLine("({0}, {1})", i, j);
                 }
 
             }
             Console.ReadKey();
-            // cum fac sa am scrol bar mai mare sa pot vizualiza toare rezultatele?
+            // cum fac sa am scrol bar mai mare sa pot vizualiza toare rezultatele?----> vezi proprietati consola
         }
     }
 }
