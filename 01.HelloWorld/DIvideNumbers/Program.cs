13ab68ac586a923e574f18a3af45db85250b4de6
iulianpas <iulian.pastrama@cegeka.com>
2016-08-29 14:33:19 +0300

    week 1 lab

---
+++ 01.HelloWorld/DIvideNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIvideNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num1 = 26;
+            int Num2 = 7;
+            int ResultInt = Num1 / Num2;
+            int ResultRemainder = Num1 % Num2;
+            float Num3 = 26;
+            float Num4 = 7;
+            float ResultWithDecimal = Num3 / Num4;
+            Console.WriteLine("26:7 is {0} and the remainder is {1}", ResultInt, ResultRemainder);
+            Console.WriteLine("26:7 is {0}", ResultWithDecimal);
+            Console.ReadKey();
+        }
+    }
+}
