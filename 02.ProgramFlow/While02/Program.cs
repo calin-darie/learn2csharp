f4556a8cc3a4d3ad52f173b67c191f35faedd068
mihaeladima <mihaela.dima@cegeka.com>
2016-08-30 11:13:09 +0300

    Lab 01 - Exercise 4 - While 02 commit 1

---
+++ 02.ProgramFlow/While02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int sum = 0;
+
+            do
+            {
+                try
+                {
+                     number = int.Parse(Console.ReadLine());
+                     if (number == 0)
+                         break;
+
+                    sum += number;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error!!! Type only numbers, please.");
+                }
+            } while (true);
+           
+            Console.WriteLine("Sum of the above numbers is:{0}", sum);
+        }
+    }
+}
