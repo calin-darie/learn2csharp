c9c5293e899edc30985dc82f83b34613481fde18
mihaeladima <mihaela.dima@cegeka.com>
2016-08-31 17:32:07 +0300

    Lab 02 - Exercise 7 - For02 - commit 1

---
+++ 02.ProgramFlow/For02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Produsul cartezian este format din urmatoarele perechi de numere:");
+            for (int i = 0; i < 21; i++)
+            {
+                for (int j = 20; j < 41; j++)
+                {
+                    Console.Write("({0},{1}); ", i, j);
+                }
+            }
+        }
+    }
+}
