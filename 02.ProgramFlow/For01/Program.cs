62d45c216a8872fc176cd42f870d3a955b65496d
mihaeladima <mihaela.dima@cegeka.com>
2016-08-31 16:50:00 +0300

    Lab 02 - Exercise 6 - For01 - commit 1

---
+++ 02.ProgramFlow/For01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //program is only for happy flow, I have to learn how to treat corner cases/exceptions
+
+            Console.WriteLine("Input a positive number from the keyboard.");
+            int number = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i <= number; i+=2)
+            {
+                Console.WriteLine("Even number between 0 and {0} is: {1}",number,i);
+            }
+       }
+    }
+}
