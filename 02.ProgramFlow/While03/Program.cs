6034a92b075f550488e1b1f362473460767189a8
mihaeladima <mihaela.dima@cegeka.com>
2016-08-31 09:05:42 +0300

    Lab 02 - Exercise 5 - While03 - commit 1

--- 02.ProgramFlow/While03/Program.cs
+++ 02.ProgramFlow/While03/Program.cs
@@ -12,10 +12,28 @@ namespace While03
         {
             Random r = new Random();
             int generatedNumber = r.Next(100); //random number in range [0-99]
+            int number;
 
-            //write your solution here.
+            Console.WriteLine("Generated number = {0}", generatedNumber);
+            Console.WriteLine("Please input a number between 0 and 99, inclusively.");
+            number = int.Parse(Console.ReadLine());
 
-
+            while (number != generatedNumber)
+            {
+                if (number > generatedNumber)
+                {
+                    Console.WriteLine("It's bigger. Try another.");
+                    number = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    if (number < generatedNumber)
+                    {
+                        Console.WriteLine("It's smaller. Try another");
+                        number = int.Parse(Console.ReadLine());
+                    }
+                }
+            }
 
             Console.WriteLine("Congratulations! You guessed the number!");
             Console.ReadKey();
