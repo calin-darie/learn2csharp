b4d3a0e259980be5597bee0fa2c26fe894da5a7a
mihaeladima <mihaela.dima@cegeka.com>
2016-08-31 18:09:49 +0300

    Lab 02 - Exercise 8 - For03 - commit 1

---
+++ 02.ProgramFlow/For03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= number; i++)
+            {
+
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine("Number {0} is divisible by ten.", i);
+                }
+                else
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine("Number {0} is divisible by five.", i);
+                    }
+                    else if (i % 2 == 0)
+                    {
+                        Console.WriteLine("Number {0} is divisible by two.", i);
+                    }
+                }
+            }
+            
+        }
+    }
+}
