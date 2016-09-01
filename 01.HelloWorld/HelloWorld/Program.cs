5d163cfb277b3fdc3000a1e5f0dc7459db4bad13
FlorinSora <sora_florin@yahoo.com>
2016-09-01 15:47:45 +0300

    just a test commit

--- 01.HelloWorld/HelloWorld/Program.cs
+++ 01.HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Florin!");
+            Console.WriteLine("Hello, Florinache!");
             Console.ReadKey();
         }
     }
