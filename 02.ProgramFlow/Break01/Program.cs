649c86a2fac0d081fc76ce177746ec5b1e2d7f54
mihaeladima <mihaela.dima@cegeka.com>
2016-09-01 12:30:10 +0300

    Lab 02 - Exercise 9 - Break01 - commit 1

---
+++ 02.ProgramFlow/Break01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            while (true)
+            {
+                Console.WriteLine("Type number to be added to the sum:");
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+                Console.WriteLine("The sum is {0}.", sum);
+                
+                if (sum > 100)
+                {
+                    Console.WriteLine("Sum is greter than 100.");
+                    break;
+                }
+                
+            }
+        }
+    }
+}
