8f59517792c6f7602091f36eb38a7099695daa33
mihaeladima <mihaela.dima@cegeka.com>
2016-09-01 12:47:04 +0300

    Lab 02 - Exercise 10 - Switch01 - commit 1

---
+++ 02.ProgramFlow/Switch01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type the name of a person: ");
+            string person = Console.ReadLine();
+            
+            switch (person)
+            {
+                case "emma":
+                    Console.WriteLine("Emma likes reading.");
+                    break;
+                case "anna":
+                    Console.WriteLine("Anna loves travelling.");
+                    break;
+                case "dan":
+                    Console.WriteLine("Dan likes playing football.");
+                    break;
+                case "john":
+                    Console.WriteLine("John is sleeping a lot.");
+                    break;
+                default:
+                    Console.WriteLine("I don't know anything about {0}", person);
+                    break;
+            }
+
+        }
+    }
+}
