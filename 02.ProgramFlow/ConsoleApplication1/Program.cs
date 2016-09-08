5b1d54e76ba3a2d62b690e097fe65a52334cd21c
Roxana Neagu <Roxana.Neagu@cegeka.com>
2016-09-08 16:41:12 +0300

    partial LAB2 (IFs)

---
+++ 02.ProgramFlow/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your fruit:"); 
+            string fruitString = Console.ReadLine();
+            string fruit = fruitString;
+            //Console.WriteLine("Your fruit color is:");
+            //string fruitcolourString = Console.ReadLine();
+            //string fruitcolour = fruitcolourString;
+
+            if (fruit == "apple")
+               {
+                Console.WriteLine("What is your fruit colour?");
+                string fruitcolourString = Console.ReadLine();
+                string fruitcolour = fruitcolourString;
+               
+                if (fruitcolour == "green" )
+            {
+                Console.WriteLine("Green apples are my favourite");
+            }
+                if (fruitcolour == "red")
+                    {
+                Console.WriteLine("Snow White likes red apples");
+            }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
+ // ar fi putut fi facut cu else if? Cum? de investigat ... 
--- 02.ProgramFlow/If01/Program.cs
+++ 02.ProgramFlow/If01/Program.cs
@@ -14,12 +14,20 @@ namespace If01
             string ageString = Console.ReadLine();
             int age = int.Parse(ageString);
 
-            Console.WriteLine("Your sex:"); //male or female
-            string sex = Console.ReadLine();
+            Console.WriteLine("Your gender:"); //male or female
+            string genderString = Console.ReadLine();
+            string gender = genderString;
 
             //write here your solution
-
-
+            if (age == 24 && gender == "male" || age == 20 && gender == "female" )
+            {
+                Console.WriteLine("Valid Person");
+            }
+            else { 
+                Console.WriteLine("Invalid Person");
+            }
+            
+            
             Console.ReadKey();
         }
     }
