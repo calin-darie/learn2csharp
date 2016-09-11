77c961797f4794d85ebad78563b602146f4779f8
AndreeaMazere <andreea.mazere@cegeka.com>
2016-09-11 17:23:17 +0300

    Gigi

---
+++ 01.HelloWorld/GigiProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigiProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kilomenters = 10;
+            double time = 1.5;
+
+                Console.WriteLine ("Gigi has ran 10 kilometers in 1.5 hours");
+
+         int kilometers && double time 
+        }
+    }
+}
