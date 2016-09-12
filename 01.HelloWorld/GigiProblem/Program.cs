dd0632a2c36cf3ce722256c39e6124d68366b47d
AndreeaMazere <andreea.mazere@cegeka.com>
2016-09-12 14:48:29 +0300

    lab 1 Gigi

--- 01.HelloWorld/GigiProblem/Program.cs
+++ 01.HelloWorld/GigiProblem/Program.cs
@@ -4,18 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GigiProblem
+namespace Gigi
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int kilomenters = 10;
+            int kilometers = 10;
             double time = 1.5;
+            Boolean truth = true;
 
-                Console.WriteLine ("Gigi has ran 10 kilometers in 1.5 hours");
+            Console.WriteLine("Gigi has ran 10 kilometers in 1.5 hours.");
 
-         int kilometers && double time 
+            Console.WriteLine("It is {true} that Gigi is a family man");
+
+            Console.WriteLine("His namespace will be Gigi, just like his father's.");
+
+            Console.WriteLine("Gigi dreams to get a diploma from his son saying {Gigi father of the year.}");
+
+
+
+
         }
     }
 }
--- 01.HelloWorld/GigiProblem/Properties/AssemblyInfo.cs
+++ 01.HelloWorld/GigiProblem/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("While03")]
+[assembly: AssemblyTitle("GigiProblem")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("While03")]
+[assembly: AssemblyProduct("GigiProblem")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("279817af-b3a4-4767-8b25-a9ec97f1f398")]
+[assembly: Guid("4d4cf1ba-4214-4440-8f16-21755083ff3b")]
 
 // Version information for an assembly consists of the following four values:
 //
