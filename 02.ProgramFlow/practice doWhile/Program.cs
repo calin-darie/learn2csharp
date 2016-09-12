146f06aa07d45c483e20229d8919e1f2e7c3fab4
AndreeaMazere <andreea.mazere@cegeka.com>
2016-09-12 14:06:22 +0300

    rezolvarea lab 2 partial

---
+++ 02.ProgramFlow/practice doWhile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_doWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[Y]es or [N]o?"); 
+            char answer; 
+            do 
+            {
+                answer = Console.ReadKey().KeyChar; 
+            } 
+            while (answer != 'y' && answer != 'n');
+
+            Console.WriteLine("Choose a number");
+            var number = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Type one for i as well");
+            var i = int.Parse (Console.ReadLine());
+
+            while (i > number)
+            {
+                Console.WriteLine("i is the best");
+                i++;
+            }
+        }
+    }
+}
