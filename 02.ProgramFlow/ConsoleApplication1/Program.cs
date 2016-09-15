26802a7cdaaa231ff03c6f184934194aae8f9db5
Roxana Neagu <Roxana.Neagu@cegeka.com>
2016-09-15 16:14:44 +0300

    IF02 exercise completed

--- 02.ProgramFlow/ConsoleApplication1/Program.cs
+++ 02.ProgramFlow/ConsoleApplication1/Program.cs
@@ -10,31 +10,81 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Your fruit:"); 
-            string fruitString = Console.ReadLine();
+            Console.WriteLine("Your fruit is:"); 
+            var fruitString = Console.ReadLine();
             string fruit = fruitString;
-            //Console.WriteLine("Your fruit color is:");
-            //string fruitcolourString = Console.ReadLine();
-            //string fruitcolour = fruitcolourString;
-
+         
             if (fruit == "apple")
-               {
-                Console.WriteLine("What is your fruit colour?");
-                string fruitcolourString = Console.ReadLine();
-                string fruitcolour = fruitcolourString;
-               
-                if (fruitcolour == "green" )
             {
-                Console.WriteLine("Green apples are my favourite");
-            }
-                if (fruitcolour == "red")
-                    {
-                Console.WriteLine("Snow White likes red apples");
+                Console.WriteLine("What is your fruit colour?");
+                var fruitcolourString = Console.ReadLine();
+                string fruitcolour = fruitcolourString;
+
+                if (fruitcolour == "green")
+                { 
+                    Console.WriteLine("Green apples are my favourite");
+                }
+                else if (fruitcolour == "red")
+                {
+                    Console.WriteLine("Snow White likes red apples");
+                }
+
+                else
+                {
+                    Console.WriteLine("I do not know such kinds of apples.");
+                }
+              
             }
 
-                Console.ReadKey();
+            else if (fruit == "orange")
+            {
+                    Console.WriteLine("IS your orange sweet or sour?");
+                    var fruittasteString = Console.ReadLine();
+                    string fruittaste = fruittasteString;
+
+                    if (fruittaste == "sweet")
+                    {
+                        Console.WriteLine("Sweet oranges are the best");
+                    }
+
+                    else if (fruittaste == "sour")
+                    {
+                        Console.WriteLine("IS your orange big or small?");
+                        var fruitsizeString = Console.ReadLine();
+                        string fruitsize = fruitsizeString;
+
+                        if (fruitsize == "big")
+                        {
+                            Console.WriteLine("Congrats!You have a big sour orange!");
+                        }
+                        else if (fruitsize == "small")
+                        {
+                            Console.WriteLine("Maybe you have a lime instead of an orange");
+                        }
+                    
+                                        }
+                                    /* else if (fruittaste != "sweet" || fruittaste != "sour")
+                                   {
+                                       Console.WriteLine("Let's start over. Your fruit is: ");
+                                       fruitString = Console.ReadLine();
+                                       fruit = fruitString;
+                                   } */
+                                    /*Nu imi place cum se termina programul. Cum fac sa se intoarca la "Your fruit is:" */ /*maybe ceva cu do...while */
+                    else
+                     {
+                        Console.WriteLine("Start all over! Start again the program?");
+                     }
             }
+            
+
+          else
+           { 
+              Console.WriteLine(fruit + " is not on our list. I like apples and oranges");
+           }
+            
+              Console.ReadKey();
+           }
         }
     }
-}
- // ar fi putut fi facut cu else if? Cum? de investigat ... 
+
+ 
