ab392f57a98c9e996f4c37a7348808e3328c2e72
AlexVladCGK <vlad.dana.alexandra@gmail.com>
2016-10-24 15:55:33 +0300

    ex 4,5,6

---
+++ 03.Objects/CircleCollisionDetection/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CircleCollisionDetection
+{
+    public class Point
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        //alta metoda outside static main si apoi in afara clasei program ci in clasa point
+        /// <summary>
+        /// calculates the distance from the current point to the point that is given as parameter instead of 4 double values from Point class; 
+        /// return distance; to test this use the method of the triangle: 3, 4, 5
+        /// </summary>
+        /// <returns></returns> 
+        public double DistanceTo(Point otherPoint)
+        {
+            var distance = Math.Sqrt((this.X - otherPoint.X) * (this.X - otherPoint.X) + (this.Y - otherPoint.Y) * (this.Y - otherPoint.Y));
+            return distance;
+        }
+
+        public Point(double x, double y) 
+        {   
+            X = x;
+            Y = y;
+        }
+
+        public Point(){  }    
+    }
+
+}
--- 03.Objects/CircleCollisionDetection/Program.cs
+++ 03.Objects/CircleCollisionDetection/Program.cs
@@ -3,19 +3,28 @@
 namespace CircleCollisionDetection
 {
     class Program
-    {
+    {//in clasa program pun toate metodele
         static void Main()
-        {   //ex1
-            Console.WriteLine("Describe a circle");
-            Console.WriteLine("Enter center coordinates");
+        {   //in static void main am ce se executa; in afara ei dar totusi in program se pun restul metodelor
+            //ex1
+            Console.WriteLine("Describe the initial circle");
+            Console.WriteLine("Enter center coordinates for initial circle");
             double circleCenterX = ReadDouble("x0: ");
             double circleCenterY = ReadDouble("y0: ");
-            double radius = ReadDouble("radius0: ");
+            double radius = ReadDouble("radiusinItialCircle: ");
 
-            Console.WriteLine("Enter click coordinates");
-            double clickPointX = ReadDouble("x: ");
-            double clickPointY = ReadDouble("y: ");
-            
+            Console.WriteLine("Enter click coordinates of the click");
+            double clickPointX = ReadDouble("xclick: ");
+            double clickPointY = ReadDouble("yclick: ");
+
+            //clasa e un template pt obiecte; obiectele apar cand faci new pe obiectele respective= instantiere a clasei sau cerare a obiectului
+            var circleCenter = new Point 
+                { 
+                    X = circleCenterX, 
+                    Y = circleCenterY
+                };//intre acolade am initializare
+            //var clickPoint= new Point {X= clickPointX, Y= clickPointY};//asta e varianta in care nu am definit construvctor in clasa Point; si asta si urmatoarea linie sunt valide
+            var clickPoint = new Point(clickPointX, clickPointY);//aici apelez cosntructorul din clasa Point pt a crea un obiect de tip point
 
             //where was the click comparing with the circle?-varianta 1
             // var distance1 = DistanceBetweenPoints (clickPointX, clickPointY, circleCenterX, circleCenterY  )
@@ -33,7 +42,7 @@ namespace CircleCollisionDetection
             //}
 
             //where was the click comparing with the circle?-varianta 2 care verifica doar 2 varinate nu 3 ca cea anterioara
-            var distance1 = DistanceBetweenPoints(clickPointX, clickPointY, circleCenterX, circleCenterY);
+            var distance1 = circleCenter.DistanceTo(clickPoint);
             bool wasClickOnCircle = (distance1 <= radius);//asociez lui wasClickOnCircle valoarea de adevar din comparatia dintre distanta si radius
             var clickMessage = wasClickOnCircle ? //daca e adevarata linia anterioara, lui clickMessage i se va asocia stringul de pe linia imediat urmatoare; altfel i se asociaza stringul de pe a doua linie urmatoare
                 "Click was inside the circle." :
@@ -44,9 +53,9 @@ namespace CircleCollisionDetection
 
             //ex 2
             Console.WriteLine("Describe another circle");
-            double otherCircleCenterX = ReadDouble("x1: ");
-            double otherCircleCenterY = ReadDouble("y1: ");
-            double otherCircleRadius = ReadDouble("radius1: ");
+            double otherCircleCenterX = ReadDouble("xanothercircle: ");
+            double otherCircleCenterY = ReadDouble("yanothercircle: ");
+            double otherCircleRadius = ReadDouble("radiusAnotherCircle: ");
 
             // did the circles collide? varianta 1
             if (Math.Sqrt((otherCircleCenterX - circleCenterX) * (otherCircleCenterX - circleCenterX) + (otherCircleCenterY - circleCenterY) * (otherCircleCenterY - circleCenterY)) == (otherCircleRadius + radius))
@@ -72,9 +81,9 @@ namespace CircleCollisionDetection
             Console.WriteLine("#######################end of ex2##############################");
             
             Console.ReadKey();
-        }
+        }//aici se iese din main
 
-
+        //alta metoda outside static main
         /// <summary>
         /// reads a string from the console and tries to parse it as a double. 
         /// if it fails, it retries indefinetly.
@@ -99,11 +108,18 @@ namespace CircleCollisionDetection
             return value;
         }
 
-        
-        static double DistanceBetweenPoints(double pointX, double pointY, double otherPointX, double otherPointY)
-        {
-            var distance = Math.Sqrt((pointX - otherPointX)*(pointX - otherPointX) + (pointY - otherPointY)*(pointY - otherPointY));
-            return distance;
-        }
-    }
-}     
+        //alta metoda outside static main
+        /// <summary>
+        /// calculates the distance between two points
+        /// return distance; to test this use the method of the triangle: 3, 4, 5
+        /// </summary>
+        /// <returns></returns>     
+        //static double DistanceBetweenPoints(double pointX, double pointY, double otherPointX, double otherPointY)
+        //{
+        //    var distance = Math.Sqrt((pointX - otherPointX)*(pointX - otherPointX) + (pointY - otherPointY)*(pointY - otherPointY));
+        //    return distance;
+        //}
+
+       
+    }// aici se iese din program
+}  //aici se iese din namespace   
