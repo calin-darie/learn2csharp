57f9455667faefba229b10e403f389cd33321f85
andreeam <andreea.mazere.cegeka@hotmail.com>
2016-11-18 17:06:48 +0200

    For01

---
+++ 02.ProgramFlow/For01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a positive number:");
+            var numberString=Console.ReadLine();
+            var number = int.Parse(numberString);
+
+            int x;
+
+            for(x=0; x <= number; x++ )
+            {
+                if (x % 2 == 0)
+                    Console.WriteLine(x);
+            
+            }
+        }
+    }
+}
+*//numbers between 0 and that number**// number=0; number>=x>0
