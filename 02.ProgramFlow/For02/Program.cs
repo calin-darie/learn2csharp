ac17e1ee34b9a74916202628e5cb4f848d5a02ed
andreeam <andreea.mazere.cegeka@hotmail.com>
2016-11-21 14:14:26 +0200

    For01

---
+++ 02.ProgramFlow/For02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int j;
+            for (i = 0; i <= 20; i++)
+            for (j = 20; j <= 40; j++) 
+            {
+                Console.WriteLine("{0},{1}", i,j);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
