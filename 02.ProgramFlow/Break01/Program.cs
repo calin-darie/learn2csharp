1abe493745f652297567638b20d54ae78bae7ec1
andreeam <andreea.mazere.cegeka@hotmail.com>
2016-11-28 13:45:35 +0200

    Break01

---
+++ 02.ProgramFlow/Break01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            string answer;
+            var number = 0;
+
+            while (true)
+            {
+                
+                Console.WriteLine("Type a number");
+                answer = Console.ReadLine();
+                number = int.Parse(answer);
+                sum = sum + number;
+                Console.WriteLine("Actual sum is {0}", sum);
+                if (sum > 100)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
--- 02.ProgramFlow/For03/Program.cs
+++ 02.ProgramFlow/For03/Program.cs
@@ -12,9 +12,9 @@ namespace For03
         {
             Console.WriteLine("Enter a number");
             string answer = Console.ReadLine();
-            var number = int.Parse (answer);
+            var number = int.Parse(answer);
             int x;
-            for (x =0; x <= number; x++)
+            for (x = 0; x <= number; x++)
                 if (x % 10 == 0)
                     Console.WriteLine("Number {0} is divisible by ten", x);
                 else if (x % 2 == 0)
@@ -22,7 +22,8 @@ namespace For03
                 else if (x % 5 == 0)
                     Console.WriteLine("Number {0} is divisible by five", x);
 
-            
+
+            Console.WriteLine();
         }
     }
 }
