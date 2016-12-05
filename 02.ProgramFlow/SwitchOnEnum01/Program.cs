bc34fedaee12f3ad84e104fdfa552422326880fa
andreeam <andreea.mazere.cegeka@hotmail.com>
2016-12-05 14:19:21 +0200

    switch on enum trials

---
+++ 02.ProgramFlow/SwitchOnEnum01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchOnEnum01
+{
+    enum Day
+    {
+        Mon, Tue, Wed, Thu, Fri, Sat, Sun
+    }
+    enum OneValue
+    {
+        Default
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day day = Day.Sat;
+            
+            OneValue one = OneValue.Default;
+            
+            if(day == Day.Mon)
+            {
+                Console.WriteLine("A inceput saptamana!\n");
+            }
+
+            switch(day)
+            {
+                case Day.Mon:
+                    Console.WriteLine("Azi e luni!\n");
+                    break;
+                case Day.Tue:
+                    Console.WriteLine("Azi e marti!\n");
+                    break;
+                default:
+                    Console.WriteLine("Azi nu e nici marti, nici luni!\n");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02.ProgramFlow/SwitchonEnum001/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchonEnum001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var  Date = Console.ReadLine();
+            enum season
+            {
+                Winter;Spring; 
+            }
+
+        }
+    }
+}
