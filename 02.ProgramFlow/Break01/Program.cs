f664a0e45a8cb8706f77085dcdff2a1307ad2ea4
roxanadn <roxana.neagu@cegeka.com>
2017-05-23 09:49:47 +0300

    Break exercise

---
+++ 02.ProgramFlow/Break01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            while (true)
+            {
+                Console.WriteLine("Enter a Number: ");
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+                {
+                    if (sum > 100)
+
+                        break;
+
+                }
+            }
+            Console.WriteLine("The sum is: " + sum);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
