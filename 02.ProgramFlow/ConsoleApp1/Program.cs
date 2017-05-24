c8cebbaa7de5ac395fa746147d5d509ce4253a00
roxanadn <roxana.neagu@cegeka.com>
2017-05-24 10:22:44 +0300

    Switch01 done

---
+++ 02.ProgramFlow/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name of a person (pick Alex, Andrew or Danielle): ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "Alex":
+                    Console.WriteLine("Alex likes skiing");
+                    break;
+
+                case "Andrew":
+                    Console.WriteLine("Andrew likes reading books");
+                    break;
+
+                case "Danielle":
+                    Console.WriteLine("Danielle likes C# programming");
+                    break;
+
+
+                default:
+                    Console.WriteLine($"I don’t know anything about {choice}");
+                    break;
+
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+    }
+}
